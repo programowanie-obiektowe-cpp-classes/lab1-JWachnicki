743dc3863f93c21803603d1f88d839d439638157
JWachnicki <julek.wachnicki@wp.pl>
2022-10-24 22:12:37 +0200

    Wektor

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,37 @@
 class Wektor2D
 {
-    print("Hello");
+    public:
+        Wektor2D(){x=0.;y=0.;}
+        Wektor2D(double u,double v)
+        {
+            x=u;
+            y=v;
+        }
+
+    void setX(double a){x=a;}
+    void setY(double b){y=b;}
+
+    double getX(){return x;}
+    double getY(){return y;}
+
+    void print()
+    {
+        std::cout<<"X:"<<x<<"\t Y:"<<y<<"\n";
+    }
+
+    Wektor2D operator + (Wektor2D &vec)
+    {
+         Wektor2D v;
+         v.setX(x+vec.getX());
+         v.setY(y+vec.getY());
+         return v;
+    }
+    double operator * (Wektor2D &v1)
+    {
+        double product = x*v1.getX()+y*v1.getY();
+        return product;
+    }
+    private:
+    double x;
+    double y; 
 };
